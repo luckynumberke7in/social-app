f9f4c1a1d644f4a4926ea11a6a6ee347f1a8a023
Kevin Wezenter <kevinwezenter@gmail.com>
2020-10-07 15:55:24 -0400

    set up React app, dependencies, concurrently (run server and react) and some generic CSS -- tested

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<section className="landing">
+			<div className="dark-overlay">
+				<div className="landing-inner">
+					<h1 className="x-large">MERN Stack Social App</h1>
+					<p className="lead">
+						Create a profile and connect with a community. Talk about
+						development, politics, whatever you like.
+					</p>
+					<div className="buttons">
+						<a href="register.html" className="btn btn-primary">
+							Sign Up
+						</a>
+						<a href="login.html" className="btn btn-light">
+							Login
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Landing;

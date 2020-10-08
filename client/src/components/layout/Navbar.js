57a9d3a8125b3689cec96b9e9bd21d969e894eab
Kevin Wezenter <kevinwezenter@gmail.com>
2020-10-08 13:08:45 -0400

    added home, login, and register pages and basic functionality -- tested

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
 		<nav className="navbar bg-dark">
 			<h1>
-				<a href="index.html">
-					<i className="fas fa-code"></i> LuckyNumberKe7in
-				</a>
+				<Link to="/">
+					<i className="fas fa-code"></i> Ke7in's Social App
+				</Link>
 			</h1>
 			<ul>
 				<li>
-					<a href="profiles.html">Developers</a>
+					{/* Change this later */}
+					<Link to="!#">Developers</Link>
 				</li>
 				<li>
-					<a href="register.html">Register</a>
+					<Link to="/register">Register</Link>
 				</li>
 				<li>
-					<a href="login.html">Login</a>
+					<Link to="/login">Login</Link>
 				</li>
 			</ul>
 		</nav>

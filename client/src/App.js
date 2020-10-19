07324bfd0a306557038f5de362c9bc709c40614b
Kevin Wezenter <kevinwezenter@gmail.com>
2020-10-19 15:48:25 -0400

    create profile form added, added onChange to fill form but no onSubmit yet

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Style
@@ -35,6 +36,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
